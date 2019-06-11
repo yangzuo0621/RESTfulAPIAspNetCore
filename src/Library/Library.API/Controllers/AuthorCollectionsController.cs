a5a29622c1ceb420bbe015ffd8c21093c711d8ef
Yang Zuo <zyang0508@163.com>
2019-06-11 23:10:57 +0800

    add create collection of authors api

---
+++ src/Library/Library.API/Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using Library.API.Models;
+using Library.API.Services;
+using Library.API.Entities;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authorcollections")]
+    [ApiController]
+    public class AuthorCollectionsController : ControllerBase
+    {
+        private readonly IAuthorRepository _authorRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorCollectionsController(IAuthorRepository authorRepository, IMapper mapper)
+        {
+            _authorRepository = authorRepository;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateAuthorCollection(
+            [FromBody] IEnumerable<AuthorForCreationDto> authorCollection)
+        {
+            if (authorCollection == null)
+            {
+                return BadRequest();
+            }
+
+            var authorEntities = _mapper.Map<IEnumerable<Author>>(authorCollection);
+
+            foreach (var author in authorEntities)
+            {
+                await _authorRepository.AddAuthorAsync(author);
+            }
+
+            if (!await _authorRepository.SaveChangesAsync())
+            {
+                throw new Exception("Creating an author collection failed on save.");
+            }
+
+            return Ok();
+        }
+    }
+}
