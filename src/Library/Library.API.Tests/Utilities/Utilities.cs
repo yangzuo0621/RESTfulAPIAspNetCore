e8935191781ccf3ad64397bcc343306c8a7995fe
Yang Zuo <zyang0508@163.com>
2019-06-12 20:26:10 +0800

    supply a DbContextOptions instance for db context

---
+++ src/Library/Library.API.Tests/Utilities/Utilities.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Library.API.Contexts;
+
+namespace Library.API.Tests
+{
+    public static class Utilities
+    {
+        public static DbContextOptions<LibraryContext> TestDbContextOptions()
+        {
+            // Create a new service provider to create a new in-memory database.
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            // Create a new options instance using an in-memory database and 
+            // IServiceProvider that the context should resolve all of its 
+            // services from.
+            var builder = new DbContextOptionsBuilder<LibraryContext>()
+                .UseInMemoryDatabase("InMemoryDb")
+                .UseInternalServiceProvider(serviceProvider);
+
+            return builder.Options;
+        }
+    }
+}
