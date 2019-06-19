15dcc5c61ef78827dad924e6a1769b25730ffc3d
Yang Zuo <zyang0508@163.com>
2019-06-19 13:33:29 +0800

    add filter for return object

---
+++ src/Library/Library.API/Filters/BookResultFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using AutoMapper;
+
+namespace Library.API.Filters
+{
+    public class BookResultFilterAttribute : ResultFilterAttribute
+    {
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            var resultFromAction = context.Result as ObjectResult;
+            if (resultFromAction?.Value == null
+                || resultFromAction.StatusCode < 200
+                || resultFromAction.StatusCode >= 300)
+            {
+                await next();
+                return;
+            }
+
+            resultFromAction.Value = Mapper.Map<Entities.Book>(resultFromAction.Value);
+        }
+    }
+}
